6a6b8afd82e1ecb83676e8a741060a89934fa3c5
John Carlson <jawnnypoo@gmail.com>
2017-05-17 00:47:23 -0500

    Execute commands properly, waiting on completion

--- README.md
+++ README.md
@@ -14,19 +14,24 @@ gem install android-sdk-installer
 The installer looks for a `android-sdk-installer.yml` file in order to configure where the artifacts are and where they should go.
 For example:
 ```yml
-version: 25.2.3
 platform: linux
 components:
   - build-tools;25.0.3
   - platforms;android-25
-  - extras;android;m2repository
-  - extras;google;m2repository
-
+  - extras;android;m2repository #support libs
+  - extras;google;m2repository #google play services lib
 ```
 After creating this configuration, all you need to do is run:
 ```shell
 android-sdk-installer
 ```
+
+## Version
+Version is the SDK Tools version, defaulting to 26.0.2 (3859397). The latest version number can be found at the bottom of the page [here](https://developer.android.com/studio/index.html) as a part of the download URL.
+
+## Platform
+Currently supported platforms are `linux` and `darwin` (macosx). `windows` is not supported. Defaults to `linux`
+
 ## Components
 The components are defined by the [sdkmanager](https://developer.android.com/studio/command-line/sdkmanager.html) command line tool. You can get a list of available components by running:
 ```shell
--- lib/android-sdk-installer.rb
+++ lib/android-sdk-installer.rb
@@ -12,38 +12,44 @@ module AndroidInstaller
     end
 
     def install
+      # Why is it formatted like this?
       key_sdk_tools = '{ANDROID_SDK_TOOLS}'
       key_platform = '{PLATFORM}'
       sdk_url = 'https://dl.google.com'
-      sdk_path = '/android/repository/tools_r' + key_sdk_tools + '-' + key_platform + '.zip'
+      sdk_path = '/android/repository/sdk-tools-' + key_platform + '-' + key_sdk_tools + '.zip'
       # Validation
       unless File.file?('android-sdk-installer.yml')
         puts "\nNo config file found. You need a file called `android-sdk-installer.yml` with the configuration. See the README for details\n\n"
         exit(1)
       end
       config = YAML.load_file('android-sdk-installer.yml')
-      unless config.has_key?('version')
-        puts "\nError in config file! Build file must contain a `version` string.\n\n"
-        exit(1)
+      version = '3859397'
+      if config.has_key?('version')
+        version = config['version']
       end
       if config['debug']
         @@logger.level = Logger::DEBUG
         @@logger.debug('We are in debug mode')
       end
-      version = config['version']
-      platform = config['platform']
+
+      platform = 'linux'
+      if config.has_key?('platform')
+        platform = config['platform']
+      end
       sdk_path[key_sdk_tools] = version
       sdk_path[key_platform] = platform
-      @@logger.debug('Installing version ' + version + ' for platform ' + platform + ' with url ' + sdk_url + sdk_path)
-      exec('wget --quiet --output-document=android-sdk.zip ' + sdk_url + sdk_path)
-      exec('unzip -q android-sdk.zip -d android-sdk')
-      exec('rm android-sdk.zip')
+      @@logger.debug('Downloading version ' + version + ' for platform ' + platform + ' with url ' + sdk_url + sdk_path)
+      `wget --quiet --output-document=android-sdk.zip #{sdk_url + sdk_path}`
+      # TODO: error out here if file not found
+      @@logger.debug('Unzipping android-sdk.zip')
+      `unzip -q android-sdk.zip -d $PWD/android-sdk`
+      `rm android-sdk.zip`
+      `export ANDROID_HOME=$PWD/android-sdk`
       components = config['components']
       components.each do |component|
         @@logger.debug('Installing component ' + component)
-        output = exec('echo y | android-sdk/tools/bin/sdkmanager ' + component)
+        output = `echo y | $ANDROID_HOME/tools/bin/sdkmanager "#{component}"`
         @@logger.debug(output)
-        puts output
         if output.include? 'Warning'
           puts "\nError installing component " + component + "\n"
           puts output
