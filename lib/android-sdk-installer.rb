12a69f8625638b153364fdec3431d131b584962a
John Carlson <jawnnypoo@gmail.com>
2017-05-29 17:46:07 -0500

    Support for ignoring existing via argv

--- lib/android-sdk-installer.rb
+++ lib/android-sdk-installer.rb
@@ -1,5 +1,6 @@
 require 'psych'
 require 'logger'
+require 'optparse'
 
 module AndroidInstaller
   # Installs components for building Android projects
@@ -11,10 +12,31 @@ module AndroidInstaller
     SDK_URL = 'https://dl.google.com'
     SDK_PATH = '/android/repository/sdk-tools-' + KEY_PLATFORM + '-' + KEY_SDK_TOOLS + '.zip'
     CONFIG_FILE = 'android-sdk-installer.yml'
+    REPOSITORIES_CONFIG_FILE = '~/.android/repositories.cfg'
+    ANDROID_DIR = '~/.android'
 
-    def initialize
-      @@logger = Logger.new(STDOUT)
-      @@logger.level = Logger::WARN
+    def initialize(arguments)
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::WARN
+
+      @ignore_existing = false
+      create_options_parser(arguments)
+    end
+
+    def create_options_parser(args)
+      args.options do |opts|
+        opts.banner = 'Usage: android-sdk-installer [OPTIONS]'
+        opts.separator ''
+        opts.separator 'Options'
+        opts.on('-i', '--ignore', 'Ignore existing Android SDK, denoted by the existence of the ANDROID_HOME env variable') do
+          @ignore_existing = true
+        end
+        opts.on('-h', '--help', 'Displays help') do
+          puts opts.help
+          exit
+        end
+        opts.parse!
+      end
     end
 
     def install_command_line_sdk(platform, version)
@@ -22,20 +44,25 @@ module AndroidInstaller
       sdk_path[KEY_SDK_TOOLS] = version
       sdk_path[KEY_PLATFORM] = platform
       url = SDK_URL + sdk_path
-      @@logger.debug('Downloading version ' + version + ' for platform ' + platform + ' with url ' + url)
+      @logger.debug('Downloading version ' + version + ' for platform ' + platform + ' with url ' + url)
       `wget --quiet --output-document=android-sdk.zip #{url}`
       unless File.file?('android-sdk.zip')
         puts "\nAndroid SDK not found at url #{url}. Make sure you have the right values in your #{CONFIG_FILE}\n"
         exit(1)
       end
-      @@logger.debug('Unzipping android-sdk.zip')
-      `unzip -q android-sdk.zip -d $PWD/android-sdk`
+      @logger.debug('Unzipping android-sdk.zip')
+      `unzip -q -o android-sdk.zip -d $PWD/android-sdk`
       `rm android-sdk.zip`
       `export ANDROID_HOME=$PWD/android-sdk`
       # Gets rid of a warning
       # https://askubuntu.com/questions/885658/android-sdk-repositories-cfg-could-not-be-loaded
-      `touch ~/.android/repositories.cfg`
-      @@logger.debug('SDK base installed to ' + Dir.pwd + 'android-sdk')
+      unless Dir.exist?(ANDROID_DIR)
+        `mkdir #{ANDROID_DIR}`
+      end
+      unless File.file?(REPOSITORIES_CONFIG_FILE)
+        `touch #{REPOSITORIES_CONFIG_FILE}`
+      end
+      @logger.debug('SDK base installed to ' + Dir.pwd + '/android-sdk')
     end
 
     def install
@@ -50,8 +77,8 @@ module AndroidInstaller
         version = config['version']
       end
       if config['debug']
-        @@logger.level = Logger::DEBUG
-        @@logger.debug('We are in debug mode')
+        @logger.level = Logger::DEBUG
+        @logger.debug('We are in debug mode')
       end
 
       platform = 'linux'
@@ -59,25 +86,24 @@ module AndroidInstaller
         platform = config['platform']
       end
 
-      ignore_existing = false
       if config.has_key?('ignore_existing')
-        ignore_existing = config['ignore_existing']
-        @@logger.debug("Ignore existing set to #{ignore_existing}")
+        @ignore_existing = config['ignore_existing']
+        @logger.debug("Ignore existing set to #{@ignore_existing}")
       end
 
-      should_install = ENV['ANDROID_HOME'].nil? || ignore_existing
+      should_install = ENV['ANDROID_HOME'].nil? || @ignore_existing
       if should_install
         install_command_line_sdk(platform, version)
       else
-        @@logger.debug('ANDROID_HOME already set. Skipping command line tools install')
+        @logger.debug('ANDROID_HOME already set. Skipping command line tools install')
       end
 
       components = config['components']
       if components != nil
         components.each do |component|
-          @@logger.debug('Installing component ' + component)
+          @logger.debug('Installing component ' + component)
           output = `echo y | $ANDROID_HOME/tools/bin/sdkmanager "#{component}"`
-          @@logger.debug(output)
+          @logger.debug(output)
           if output.include? 'Warning'
             puts "\nError installing component " + component + "\n"
             puts output
--- test/test.rb
+++ test/test.rb
@@ -1,3 +1,7 @@
 require_relative '../lib/android-sdk-installer.rb'
-installer = AndroidInstaller::Installer.new
+
+input = ARGV
+puts 'Input size: ' + input.size.to_s
+puts 'Input: ' + input.to_s
+installer = AndroidInstaller::Installer.new(input)
 installer.install
